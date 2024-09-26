d2d4f3d10a75e95e2e782c64b3084da3d7e5c2f6
kalpa gamage <kalpagamage@gmail.com>
2024-09-26 12:53:44 +0100

    basic client with shortest path is completed

--- MazeClient.cpp
+++ MazeClient.cpp
@@ -1,38 +1,59 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <string>
 
 using boost::asio::ip::tcp;
-using namespace boost;
+
+class MazeClient {
+public:
+    MazeClient(boost::asio::io_context& io_context, const std::string& host, const std::string& port)
+        : socket_(io_context) {
+        tcp::resolver resolver(io_context);
+        boost::asio::connect(socket_, resolver.resolve(host, port));
+    }
+
+    void play() {
+        while (true) {
+            std::cout << "Enter command (W, S, A, D, path to nearest coin, exit to quit): ";
+            std::string command;
+            std::getline(std::cin, command);
+
+            // Check if the player wants to exit the game
+            if (command == "exit" || command == "quit") {
+                std::cout << "Exiting the game. Goodbye!\n";
+                break;  // Exit the game loop
+            }
+
+            // Send command to the server
+            boost::asio::write(socket_, boost::asio::buffer(command + "\n"));
+
+            // Read server response
+            boost::asio::streambuf response;
+            boost::asio::read_until(socket_, response, '\n');
+            std::istream is(&response);
+            std::string reply;
+            std::getline(is, reply);
+            std::cout << "Server reply: " << reply << std::endl;
+
+            // If the response indicates victory, exit the loop
+            if (reply == "Victory!\n") {
+                std::cout << "Game over, you collected all the coins!" << std::endl;
+                break;
+            }
+        }
+    }
+
+private:
+    tcp::socket socket_;
+};
 
 int main() {
     try {
-        asio::io_context io_context;
-        tcp::socket socket(io_context);
-        tcp::resolver resolver(io_context);
-        asio::connect(socket, resolver.resolve("127.0.0.1", "1234"));
-
-        std::cout << "Connected to the server!\n";
-
-        // Receive welcome message
-        char data[128];
-        size_t length = socket.read_some(asio::buffer(data));
-        std::cout << std::string(data, length) << std::endl;
-
-        while (true) {
-            // Get player input (WASD for movement)
-            char move;
-            std::cin >> move;
-
-            asio::write(socket, asio::buffer(&move, 1));
-
-            // Read server response
-            length = socket.read_some(asio::buffer(data));
-            std::cout << std::string(data, length) << std::endl;
-        }
-
+        std::cout << "Maze Client Running...." << std::endl;
+        boost::asio::io_context io_context;
+        MazeClient client(io_context, "127.0.0.1", "12345");
+        client.play();
     } catch (std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << "Exception: " << e.what() << std::endl;
     }
-
-    return 0;
 }
